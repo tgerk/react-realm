c4921beb0fb3e7575b3275ab54857b3f1c7b3d82
Tim Gerk <tjgerk@gmail.com>
2021-05-14 10:30:37 -0700

    eliminate unnecessary use of classes

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -1,57 +1,55 @@
-export default class RestaurantsController {
-  static async apiGetRestaurants(req, res, next) {
-    const { restaurants } = req.app.get("state")
+export async function apiGetRestaurants(req, res, next) {
+  const { restaurants } = req.app.get("state")
 
-    const restaurantsPerPage = req.query.restaurantsPerPage
-      ? parseInt(req.query.restaurantsPerPage, 10)
-      : 20
-    const page = req.query.page ? parseInt(req.query.page, 10) : 0
+  const restaurantsPerPage = req.query.restaurantsPerPage
+    ? parseInt(req.query.restaurantsPerPage, 10)
+    : 20
+  const page = req.query.page ? parseInt(req.query.page, 10) : 0
 
-    let filters = {}
-    if (req.query.cuisine) {
-      filters.cuisine = req.query.cuisine
-    } else if (req.query.zipcode) {
-      filters.zipcode = req.query.zipcode
-    } else if (req.query.name) {
-      filters.name = req.query.name
-    }
-
-    const {
-      restaurantsList,
-      totalNumRestaurants,
-    } = await restaurants.getRestaurants({
-      filters,
-      page,
-      restaurantsPerPage,
-    })
-
-    let response = {
-      restaurants: restaurantsList,
-      page: page,
-      filters: filters,
-      entries_per_page: restaurantsPerPage,
-      total_results: totalNumRestaurants,
-    }
-    res.json(response)
+  let filters = {}
+  if (req.query.cuisine) {
+    filters.cuisine = req.query.cuisine
+  } else if (req.query.zipcode) {
+    filters.zipcode = req.query.zipcode
+  } else if (req.query.name) {
+    filters.name = req.query.name
   }
 
-  static async apiGetRestaurantById(req, res, next) {
-    const { restaurants } = req.app.get("state")
+  const {
+    restaurantsList,
+    totalNumRestaurants,
+  } = await restaurants.getRestaurants({
+    filters,
+    page,
+    restaurantsPerPage,
+  })
 
-    let id = req.params.id || {}
-    let restaurant = await restaurants.getRestaurantByID(id)
-    if (!restaurant) {
-      res.status(404).json({ error: "Not found" })
-      return
-    }
+  let response = {
+    restaurants: restaurantsList,
+    page: page,
+    filters: filters,
+    entries_per_page: restaurantsPerPage,
+    total_results: totalNumRestaurants,
+  }
+  res.json(response)
+}
 
-    res.json(restaurant)
+export async function apiGetRestaurantById(req, res, next) {
+  const { restaurants } = req.app.get("state")
+
+  let id = req.params.id || {}
+  let restaurant = await restaurants.getRestaurantByID(id)
+  if (!restaurant) {
+    res.status(404).json({ error: "Not found" })
+    return
   }
 
-  static async apiGetRestaurantCuisines(req, res, next) {
-    const { restaurants } = req.app.get("state")
+  res.json(restaurant)
+}
 
-    let cuisines = await restaurants.getCuisines()
-    res.json(cuisines)
-  }
+export async function apiGetRestaurantCuisines(req, res, next) {
+  const { restaurants } = req.app.get("state")
+
+  let cuisines = await restaurants.getCuisines()
+  res.json(cuisines)
 }
--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -1,63 +1,57 @@
-import ReviewsDAO from "../dao/reviewsDAO.js"
+export async function apiPostReview(req, res, next) {
+  const { reviews } = req.app.get("state")
 
-export default class ReviewsController {
-  static async apiPostReview(req, res, next) {
-    const { reviews } = req.app.get("state")
+  const restaurantId = req.body.restaurant_id
+  const review = req.body.text
+  const userInfo = {
+    name: req.body.name,
+    _id: req.body.user_id,
+  }
+  const date = new Date()
 
-    const restaurantId = req.body.restaurant_id
-    const review = req.body.text
-    const userInfo = {
-      name: req.body.name,
-      _id: req.body.user_id,
-    }
-    const date = new Date()
+  const reviewResponse = await reviews.addReview(
+    restaurantId,
+    userInfo,
+    review,
+    date
+  )
 
-    const reviewResponse = await reviews.addReview(
-      restaurantId,
-      userInfo,
-      review,
-      date
-    )
+  res.json({ status: "success" })
+}
 
-    res.json({ status: "success" })
+export async function apiUpdateReview(req, res, next) {
+  const { reviews } = req.app.get("state")
+
+  const reviewId = req.body.review_id
+  const text = req.body.text
+  const date = new Date()
+
+  const reviewResponse = await reviews.updateReview(
+    reviewId,
+    req.body.user_id,
+    text,
+    date
+  )
+
+  var { error } = reviewResponse
+  if (error) {
+    res.status(400).json({ error })
   }
 
-  static async apiUpdateReview(req, res, next) {
-    const { reviews } = req.app.get("state")
-
-    const reviewId = req.body.review_id
-    const text = req.body.text
-    const date = new Date()
-
-    const reviewResponse = await reviews.updateReview(
-      reviewId,
-      req.body.user_id,
-      text,
-      date
-    )
-
-    var { error } = reviewResponse
-    if (error) {
-      res.status(400).json({ error })
-    }
-
-    if (reviewResponse.modifiedCount === 0) {
-      throw new Error(
-        "unable to update review - user may not be original poster"
-      )
-    }
-
-    res.json({ status: "success" })
+  if (reviewResponse.modifiedCount === 0) {
+    throw new Error("unable to update review - user may not be original poster")
   }
 
-  static async apiDeleteReview(req, res, next) {
-    const { reviews } = req.app.get("state")
+  res.json({ status: "success" })
+}
 
-    const reviewId = req.query.id
-    const userId = req.body.user_id
+export async function apiDeleteReview(req, res, next) {
+  const { reviews } = req.app.get("state")
 
-    const reviewResponse = await reviews.deleteReview(reviewId, userId)
+  const reviewId = req.query.id
+  const userId = req.body.user_id
 
-    res.json({ status: "success" })
-  }
+  const reviewResponse = await reviews.deleteReview(reviewId, userId)
+
+  res.json({ status: "success" })
 }
