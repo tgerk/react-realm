93514a245126a572880e92cc51ff4cd2eabae6ae
Tim Gerk <tjgerk@gmail.com>
2021-05-26 21:44:18 -0700

    extended a webhook function and deployed by CLI
    realm-cli push --local . --remote application-1-sdtbu

--- realm/Application-1-v20200603/services/Service-1/incoming_webhooks/restaurants/source.js
+++ realm/Application-1-v20200603/services/Service-1/incoming_webhooks/restaurants/source.js
@@ -1,5 +1,9 @@
 exports = async function (payload, response) {
 
+  if ("id" in payload.query) {
+    return await getOne(payload, response)
+  }
+
   const count = parseInt(payload.query.count) || parseInt(payload.query.restaurantsPerPage) || 20,
     offset = parseInt(payload.query.offset) || parseInt(payload.query.page) * count || 0;
 
@@ -17,34 +21,85 @@ exports = async function (payload, response) {
   }
 
   const db = context.services.get("mongodb-atlas").db("sample_restaurants");
-  const resultCount = await db.restaurants.find(query).count(),
-    resultPage = await db.restaurants.find(query)
-      .skip(offset)
-      .limit(count);
+  const search = db.restaurants.find(query).skip(offset).limit(count),
+    restaurantsCount = await search.clone().count(false, { limit: 9999 }),
+    restaurants = await search.toArray();
 
-  const responseData = {
-    restaurants: resultPage.map(({ _id, ...restaurant }) =>
-      ({ _id: _id.toString(), ...restaurant })
-    ),
+  const result = {
+    restaurants: restaurants.map(({ _id, ...rest }) => ({ id: _id.toString(),  ...rest })),
     filters: String(new URLSearchParams(filters)),
-    restaurantCount: resultCount
+    restaurantCount: restaurantsCount
   };
 
   if (offset > 0) {
-    responseData.first = String(new URLSearchParams({ ...filters, count }));
+    result.first = String(new URLSearchParams({ ...filters, count }));
 
     if (offset >= count) {
-      responseData.prev = String(new URLSearchParams({ ...filters, offset: max(offset - count, 0), count }));
+      result.prev = String(new URLSearchParams({ ...filters, offset: max(offset - count, 0), count }));
     }
   }
 
-  if (offset + count < resultCount) {
-    if (offset + count + count < resultCount) {
-      responseData.next = String(new URLSearchParams({ ...filters, offset: (offset + count), count }));
+  if (offset + count < restaurantsCount) {
+    if (offset + count + count < restaurantsCount) {
+      result.next = String(new URLSearchParams({ ...filters, offset: (offset + count), count }));
     }
-    
-        responseData.last = String(new URLSearchParams({ ...filters, offset: count * Math.floor(resultCount / count), count }));
+
+    result.last = String(new URLSearchParams({ ...filters, offset: count * Math.floor(restaurantsCount / count), count }));
   }
 
-  return responseData;
+  return result;
 };
+
+async function getOne(payload, response) {
+
+  const pipeline = restaurantAggregationPipeline(payload.query.id);  //TODO: uncaught exception, handle error case 400: invalid id
+
+  const db = context.services.get("mongodb-atlas").db("sample_restaurants");
+  const { _id, reviews = [], ...rest } = await db.restaurants.aggregate(pipeline).next()  //TODO: uncaught exception, handle error case 404: NOT FOUND
+
+  return {
+    id: _id.toString(),
+    ...rest,
+    reviews: reviews?.map(({ _id, date, ...rest }) => ({
+      id: _id.toString(),
+      date: new Date(date),
+      ...rest
+    }))
+  }
+}
+
+const restaurantAggregationPipeline = (id) => ([
+  {
+    $match: {
+      _id: BSON.ObjectId(id),
+    },
+  },
+  {
+    $lookup: {
+      from: "reviews",
+      let: {
+        id: "$_id",
+      },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $eq: ["$restaurant_id", "$$id"],
+            },
+          },
+        },
+        {
+          $sort: {
+            date: -1,
+          },
+        },
+      ],
+      as: "reviews",
+    },
+  },
+  {
+    $addFields: {
+      reviews: "$reviews",
+    },
+  },
+])
