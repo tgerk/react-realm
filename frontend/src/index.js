861275b1a089ec7565171560c0abc19e7341110c
Tim Gerk <tjgerk@gmail.com>
2021-06-05 12:30:09 -0700

    transition from Bootstrap to Sass

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "./components/App.jsx";
+
+import "./index.scss";
 
 ReactDOM.render(
   <BrowserRouter>
